9f2b69cb9290ed61c034826c3de307d5a2e95f7d
Hend Mazied <hendmazied@gmail.com>
2026-05-13 10:06:28 +0300

    Track all product field changes in chatter

---
+++ log_full_tracking/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Log Full Tracking",
+    "version": "1.0",
+    'depends': ['product', 'account', 'analytic', 'mail', 'mrp'],
+    "author": "hend",
+    "category": "Inventory",
+    "summary": "Track all product field changes in chatter",
+    'data': [
+            'views/chatter.xml', 
+    ],
+    "installable": True,
+    "application": False,
+}
