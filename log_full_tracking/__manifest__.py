0514b66b81129d4a40f6b1e7d31fe905b541582c
Hend Mazied <hendmazied@gmail.com>
2026-05-13 09:53:52 +0300

    log_full_tracking

---
+++ log_full_tracking/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "DVIT Log Full Tracking",
+    "version": "1.0",
+    'depends': ['product', 'account', 'analytic', 'mail', 'mrp'],
+    "author": "DVIT",
+    "category": "Inventory",
+    "summary": "Track all product field changes in chatter",
+    'data': [
+            'views/chatter.xml', 
+    ],
+    "installable": True,
+    "application": False,
+}
